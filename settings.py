6fba09ee34ef0130bda647436253d70d078bb317
Geoff Affleck <GAffleck@rbauction.com>
2018-12-01 22:14:28 -0800

    setting static root

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
 import django_heroku
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
