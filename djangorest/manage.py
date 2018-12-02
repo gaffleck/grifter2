cab95aa2562993a227bd73eb436d28736fe8cb35
Geoff Affleck <GAffleck@rbauction.com>
2018-12-01 22:17:08 -0800

    moving settings to manage

--- djangorest/manage.py
+++ djangorest/manage.py
@@ -4,6 +4,10 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangorest.settings')
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
