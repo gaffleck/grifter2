8d626700dc46526bbb3591d30831ca4280f4156f
Geoff Affleck <GAffleck@rbauction.com>
2018-12-02 17:19:48 -0800

    normpath

--- djangorest/settings.py
+++ djangorest/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+import os 
+from os.path import normpath
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -127,12 +128,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = normpath(str.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    normpath(str.join(BASE_DIR, 'static')),
 )
 
 django_heroku.settings(locals())
